ed8eddf0e53ee88650b142c5a1444030029e9eb9
IsaiasQB <isaias.qb@hotmail.com>
2022-04-18 23:03:40 -0400

    implemented and tested the prompt function for inquirer

--- Develop/index.js
+++ Develop/index.js
@@ -5,6 +5,19 @@ const inquirer = require('inquirer');
 
 // // TODO: Create an array of questions for user input
 // const questions = [];
+const askInputQuestions = () => {
+return inquirer
+  .prompt([
+      {
+        type:'input',
+        name:'appName',
+        message:'What is the name of your project/application?'
+      }
+    ])// end of prompt
+};
+
+askInputQuestions().then(answers => console.log(answers));
+
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
@@ -29,7 +42,7 @@ const inquirer = require('inquirer');
 //   console.log('test read me file generated')
 // });
 
-console.log(inquirer)
+
 
 //END of experimental code......
 
