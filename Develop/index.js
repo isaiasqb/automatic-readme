79ba3ea124cb0ea9a29700bfed79347f327a5f5d
IsaiasQB <isaias.qb@hotmail.com>
2022-04-18 22:52:17 -0400

    installed the inquirer package

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,14 @@
 // // TODO: Include packages needed for this application
-const fs = require('fs');
-const generateMarkdown = require('../Develop/utils/generateMarkdown')
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generateMarkdown = require('../Develop/utils/generateMarkdown')
+
 // // TODO: Create an array of questions for user input
 // const questions = [];
 
 // // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
+
 
 // // TODO: Create a function to initialize app
 // function init() {}
@@ -18,15 +21,15 @@ const generateMarkdown = require('../Develop/utils/generateMarkdown')
 
 
 //Experimental Code......
-const userInput = process.argv.slice(2, process.argv.length);
-const projectName = userInput;
 
+// const generateReadMeFile = generateMarkdown(data);
 
+// fs.writeFile('../dist/project.md', generateReadMeFile, err => {
+//   if (err) throw err;
+//   console.log('test read me file generated')
+// });
 
-fs.writeFile('../dist/project.md', generateMarkdown(projectName), err => {
-  if (err) throw err;
-  console.log('test read me file generated')
-});
+console.log(inquirer)
 
 //END of experimental code......
 
